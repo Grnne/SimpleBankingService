bf04012a20bce61ab2edc3120b1e7b3c5e655991
Serge Zakhary <grimglance@gmail.com>
2025-07-31 21:11:29 +0700

    добавил пустой проект с удаленными случайно отовсюду тестами

---
+++ SimpleAccountService.Tests/AutoMapper/AutoMapperUnitTests.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleAccountService.Tests.AutoMapper;
+
+public class AutoMapperUnitTests
+{
+    [Fact]
+    public void Test1()
+    {
+        // TODO повторить то что я удалил локально и глобально своими кривыми руками
+    }
+}
